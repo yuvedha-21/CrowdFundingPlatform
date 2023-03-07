f1dc66b507e96282addd2471b0927f1f798e7c50
yuvedha-21 <yuvedha21@gmail.com>
2023-03-07 12:28:19 +0530

    client end

--- crowdfunding/contracts/CrowdFunding.sol
+++ crowdfunding/contracts/CrowdFunding.sol
@@ -1,60 +1,69 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract crowdFunding {
-struct Campaign{
-    address owner;
-    string title;
-    string description;
-    uint256 target;
-    uint256 amountCollected;
-    uint256 deadline;
-    string image;
-    address[] donators;
-    uint256[] donations;
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
     }
 
-    mapping (uint256=>Campaign) public campaign ;
-    uint256 public numberOfCampaign=0;
+    mapping(uint256 => Campaign) public campaigns;
 
-    function createCampaign(address _owner,string memory _title, string memory _description,uint256 _target,uint256 _deadline, string memory _image)public returns(uint256){
-        Campaign storage campaign=campaign[numberOfCampaign];
-        require(campaign.deadline<block.timestamp,"the deadline should be less than timestamp");
-        campaign.owner=_owner;
-        campaign.title=_title;
-        campaign.description=_description;
-        campaign.target=_target;
-        campaign.deadline=_deadline;
-        campaign.amountCollected=0;
-        campaign.image=_image;
+    uint256 public numberOfCampaigns = 0;
 
-        numberOfCampaign++;
-        return(numberOfCampaign-1);
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
     }
 
-    function donateCampaign(uint256 _id)public payable{
-        uint256 amount=msg.value;
-        Campaign storage campaign=campaign[_id];
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
-        (bool sent,)=payable(campaign.owner).call{value:amount}("");
 
-        if(sent){
-            campaign.amountCollected=campaign.amountCollected+amount;
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
 
-    function getDonators(uint256 _id)view public returns(address[] memory,uint256[] memory){
-        return(campaign[_id].donators,campaign[_id].donations);
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaign()public view returns(Campaign[] memory){
-        Campaign[] memory allCompaign=new Campaign[](numberOfCampaign);
-        for(uint i=0;i<numberOfCampaign;i++){
-            Campaign storage items=campaign[i];
-            allCompaign[i]=items;
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
         }
-        return allCompaign;
-    }
 
+        return allCampaigns;
+    }
 }
