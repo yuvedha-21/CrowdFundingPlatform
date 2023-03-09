d03b2ed14a7f80649bdff837515106ab69be53c4
yuvedha-21 <yuvedha21@gmail.com>
2023-03-09 15:29:53 +0530

    Nav bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create-campaign" element={<CreateCampaign />}></Route>
+          <Route
+            path="/campaign-details/:id"
+            element={<CampaignDetails />}
+          ></Route>
         </Routes>
       </div>
     </div>
